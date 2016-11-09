6b95b05b2ea07fa7567d2df570493380475c5a5d
xiaochao <18681032630@163.com>
2016-11-09 19:45:48 +0800

    demo

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -4,10 +4,11 @@
 
 <link rel="stylesheet" type="text/css" href="{{ asset('/assets/styles/bootstrap.'.Config::get('theme.name', 'default').'.min.css') }}">
 <link rel="stylesheet" type="text/css" href="{{ asset('/css/main.css') }}">
+
 <script src="{{ asset('/js/jquery.js?t=1476958143') }}"></script>
 <script src="{{ asset('/layer/layer.js?v=2.4') }}"></script>
 <script src="{{ asset('/js/common/vilidata.js') }}"></script>
-
+<script type="text/javascript" src="http://cdn.staticfile.org/twitter-bootstrap/3.0.1/js/bootstrap.min.js"></script>
 @if(isset($media))
     @if($media['js'])
         @foreach($media['js'] as $value)
