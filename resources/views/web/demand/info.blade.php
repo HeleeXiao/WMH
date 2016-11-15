97996d758a2b11c09b133b6e0b5ee4f3d019494b
your name <zls@pccb.com>
2016-11-15 17:58:51 +0800

    xx

--- resources/views/web/demand/info.blade.php
+++ resources/views/web/demand/info.blade.php
@@ -6,6 +6,7 @@
 @section('top')
 @stop
 @section('content')
+<!-- {{dump($demand->file->toArray())}} -->
 <div class="container" style="margin:0 auto;padding: 48px 0 0 0;width: 100%;">
     <div class="album-header">
         <img class="album-header-bg ohmyblured" src="/images/banner/20161108105946_nxsJL.thumb.1200_280_g.jpeg" height="280">
@@ -13,14 +14,19 @@
         <table class="album-header-info tc">
             <tbody>
             <tr>
-                <td>
-                    <h1 class="album-title">{{ $demand->title }}</h1>
-                    <p class="album-info">
-                        <span class="album-count">{{ $demand->file->count() }}张图片</span>
-                        &nbsp;<b>·</b>&nbsp;<span class="like-count"><i>10</i>人关注</span></p>
-                    <p class="album-desc">
-                    </p>
-                </td>
+  				      <div class="outer">
+                  <div class="inner">
+
+                  </div>
+                </div>
+              <!--   <td>
+                  <h1 class="album-title">{{ $demand->title }}</h1>
+                  <p class="album-info">
+                      <span class="album-count">{{ $demand->file->count() }}张图片</span>
+                      &nbsp;<b>·</b>&nbsp;<span class="like-count"><i>10</i>人关注</span></p>
+                  <p class="album-desc">
+                  </p>
+              </td> -->
             </tr>
             </tbody>
         </table>
@@ -31,8 +37,8 @@
                 <span class="name">{{ $demand->user->name }}</span>
             </a>
             <div class="album-action dib">
-                <a title="收藏" class="albumcollectbtn" href="javascript:;" data-collect="{&quot;id&quot;:&quot;81183780&quot;,&quot;favorite_id&quot;:&quot;0&quot;}"><i></i><span>收藏</span></a>
-                <a title="赞" class="albumlikebtn " data-albumlike="{&quot;id&quot;:&quot;81183780&quot;,&quot;like_id&quot;:&quot;0&quot;}" href="javascript:;"><i></i><span>赞</span></a>
+                <a title="收藏" class="albumcollectbtn" href="javascript:;" ><i></i><span>收藏</span></a>
+                <a title="赞" class="albumlikebtn "  href="javascript:;"><i></i><span>赞</span></a>
                 <div id="album-share" class="album-share" href="javascript:;">
                     <i></i>
                 </div>
